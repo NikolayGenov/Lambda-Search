9c0d1c1735a05f3f8795492840c459907aab9fd7
NikolayGenov <nikolay.genov2@gmail.com>
2014-02-19 13:19:16 +0200

    Make it more like a gem structure

---
+++ lib/lambda-search/CLI.rb
@@ -0,0 +1,12 @@
+require './Digger'
+
+digger = Digger.new page_rank_file: "ranks", titles_file:"titles", max_results: 20
+time_start = Time.now
+puts "Enter search phrase"
+@results = digger.search(gets.chomp)
+time_end = Time.now
+@time_taken = "#{'%6.2f' % (time_end - time_start)} secs"
+@results.each_with_index do |object,index|
+
+  printf("%02d. Title: '%s' \t Link: %s\n",index,object.last,object.first)
+end
